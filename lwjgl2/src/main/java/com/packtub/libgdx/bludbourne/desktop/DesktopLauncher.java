517d6974bd6aee1a0cb0583ab578c2654bba11cb
dipankerkaul <42276172+dipankerkaul@users.noreply.github.com>
2023-02-07 21:30:38 +0530

    Desktop Launcher modified according to chapter 2

--- lwjgl2/src/main/java/com/packtub/libgdx/bludbourne/desktop/DesktopLauncher.java
+++ lwjgl2/src/main/java/com/packtub/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -1,31 +1,24 @@
 package com.packtub.libgdx.bludbourne.desktop;
 
-import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.Application;import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.packtub.libgdx.bludbourne.BludBourne;
 
 /** Launches the desktop (LWJGL) application. */
 public class DesktopLauncher {
-    public static void main(String[] args) {
-        createApplication();
-    }
-
-    private static LwjglApplication createApplication() {
-        return new LwjglApplication(new BludBourne(), getDefaultConfiguration());
-    }
-
-    private static LwjglApplicationConfiguration getDefaultConfiguration() {
-        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-        configuration.title = "bludbourne";
-        configuration.width = 640;
-        configuration.height = 480;
-        //// This prevents a confusing error that would appear after exiting normally.
-        configuration.forceExit = false;
-
-        for (int size : new int[] { 128, 64, 32, 16 }) {
-            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new  LwjglApplicationConfiguration();
+        config.title = "BludBourne";
+        config.useGL30 = false;
+        config.width = 800;
+        config.height = 600;
+        Application app = new LwjglApplication(new BludBourne(),       config);
+        Gdx.app = app;
+        //Gdx.app.setLogLevel(Application.LOG_INFO);
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Gdx.app.setLogLevel(Application.LOG_ERROR);
+        // Gdx.app.setLogLevel(Application.LOG_NONE);
         }
-        return configuration;
+
     }
-}
