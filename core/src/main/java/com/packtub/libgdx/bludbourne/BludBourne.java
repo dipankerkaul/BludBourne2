4a32c2dc6426f2b431ecf20f310f2f85827a975c
dipankerkaul <42276172+dipankerkaul@users.noreply.github.com>
2023-02-07 21:38:16 +0530

    Added libraries of utility class, but the main game class is still fucked because haven't added the screens class

--- core/src/main/java/com/packtub/libgdx/bludbourne/BludBourne.java
+++ core/src/main/java/com/packtub/libgdx/bludbourne/BludBourne.java
@@ -1,34 +1,19 @@
 package com.packtub.libgdx.bludbourne;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.packtub.libgdx.bludbourne.screens.MainGameScreen;
+import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class BludBourne extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class BludBourne extends Game {
+    public static final MainGameScreen _mainGameScreen = new MainGameScreen();
 
     @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+    public void create()
+    {    setScreen(_mainGameScreen);  }
 
     @Override
-    public void render() {
-        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+    public void dispose()
+    {    _mainGameScreen.dispose();  }
 }
+
---
+++ core/src/main/java/com/packtub/libgdx/bludbourne/Utility.java
@@ -0,0 +1,12 @@
+package com.packtub.libgdx.bludbourne;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.assets.loaders.TextureLoader;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.assets.AssetManager;
+
+public class Utility {
+}
